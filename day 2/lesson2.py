d238169c30d742a68d15dad94477206efdae901f
GiorgiChkhetiani <chkhetiani@gmail.com>
2023-01-26 10:21:13 +0400

    first commit

---
+++ day 2/lesson2.py
@@ -0,0 +1,18 @@
+name = "giorgi"
+# name  არის ცვლადი 
+# =  არის ცვლადის მნისჰვნელობის მიმნიჭებელი სიმბოლო
+#  "nika" არის ცვლადისთვის მნიშვნელობა 
+
+surname ="chkhetiani"
+#print (name)
+# პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "giorgi" #ეს არის str (string)ტიპის ცვლადი
+
+age = 22 #ეს არის int (integer)  მთელი რიცხვი
+height =  174.5  # ეს არის float ტიპის ცვლადი (ათწილადი )
+#boolean (bool) ტიპის ცვლადი
+knows_programing = True 
+is_ugly = False #snakecase (უბრალოდ წერის სტილი სტანდარტული )
+isUgly = False #ჯავასკრიპტული camelcase
+print(name + " " + surname + " " + str(age) + " " + str(height) )
