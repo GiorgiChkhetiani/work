b40f08d7c812c465c37f41569061e0c5d212387b
GiorgiChkhetiani <chkhetiani@gmail.com>
2023-01-29 00:58:41 +0400

    day4 code commit

--- day3/lesson2.py
+++ day3/lesson2.py
@@ -1,1 +1,33 @@
-print("this is a lesson 3")
+
+#print(len(name)) #len  lenght არის ფუნქცია რომელიც აბრუნებს მის ბრჩხილებშ გადაცემულლი ობიექტის სიგრძეს
+
+#""" გამოიყენება გრძელ სტრინგზე 
+#IF  არის ჩაშენებული დარეზერვებული სიტყვა
+#if შემდეგ გამოყენებული სტრინგი გამოყოფილი უნფა იყოს კედლიდან სფეისით
+#ტაბით მოვახდინეთ ინდენტაცია (indentation)
+#know_programing = true ( არის ბულიან ტიპის ცვლადი რომლის შემდეგაც იწერება true an false )
+# ყველა სტრინგი შეგვიძლია მივიჩნიოთ სიად 
+#print(full_name[0]) სტრინგიდან ინდექსის მიხედვით პოზიციის ამოღება 
+#position = index სინონიმებია ამ შემთხვევაში 0 
+#print("z" in full_name ) შეგვიძლია  არის თუ არა z full_name ში
+#full_name"= "giorgi chketiani"
+#print(full_name [2:7]) [2:7]არის დიაპაზონი რასაც ამოჭრის 
+#print(full_name[2:9:2]) მესამე რიცხვი პასუხისმგებელია ნაბიჯზე start:finish:step
+# full_name = "giorgi chkhetiani"
+# print(full_name[:5])
+# print(full_name[5:])
+
+# full_name = "giorgi chkhetiani
+
+#print(full_name[-1]) მინუსის შემთვევაში შემთვევი ბოლოდან იწყებს 
+#აქაც მოქმედებს start:finish:step ოღნდ მინუსიდან 
+
+#სტრინგებს აქვთ მეთოდები 
+# 1)სტრინგ.upper() აფერ არის მის წინ მდებარე ყველა ობიექტის გაზრდა lowerაპატარავებს 
+# 2)len(სტრინგ)
+#name2 ="    nika   "
+#print(name2.strip()) სფეისების ჩამოჭრის მეთოდი
+name2 = "giorgi chkhetiani "
+print(name2.replace("i" , "#"))
+
+
---
+++ day4/lesson4.py
@@ -0,0 +1,55 @@
+# name="giorgi"
+# print(len(name))
+# print("""ხვიჩა კვარაცხელია მსოფლიოს 
+# ამომავალი ვარსკვლავია  """ )
+
+# print(name[3])
+
+# knows_programming=True
+# if 10>5:
+#    print("ronaldo is better ")
+
+# full_name = "   giorgi chkhetiani   "
+# print(len(full_name))
+
+
+
+#print (full_name[4])
+
+
+#print(full_name [2:8])
+  
+
+#print(full_name[10 : 18 : 3])
+
+# print("z"in full_name)
+# print("g"not in full_name)
+# print("g"in full_name )
+# print("z"not in full_name)
+
+
+# print (full_name[:5])
+# print(full_name[5:])
+
+
+# print(full_name[-3])
+
+
+
+# print(full_name[-3:] +  "ko")
+
+
+
+# print(full_name[-7 : -1 : 2   ])
+
+# print(full_name.upper())
+
+# print(full_name.lower())
+
+# print(full_name.strip())
+
+# print(full_name)
+
+
+# print(""" სასიამოვნო პროცესია , 
+# დრო ისე გადის ვერ ვგებულობ. დღეს მორჩა  """)
